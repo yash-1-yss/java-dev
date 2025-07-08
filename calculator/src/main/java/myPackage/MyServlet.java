ae4d0e2bb782c53d2a4543283471ad0e1fb6f0d6
yash-1-yss <yashgupta790590@gmail.com>
2025-07-08 15:52:33 +0530

    changed

--- calculator/src/main/java/myPackage/MyServlet.java
+++ calculator/src/main/java/myPackage/MyServlet.java
@@ -28,34 +28,34 @@ public class MyServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-//		String s1=request.getParameter("number1");
-//		String s2=request.getParameter("number2");
-//		int n1=Integer.parseInt(s1);
-//		int n2=Integer.parseInt(s2);
-//		String op=request.getParameter("bt1");
-//		int o=Integer.parseInt(op);
-//		int ans;
-//		switch(o) {
-//		case 1:
-//			ans=n1+n2;
-//			break;
-//		case 2:
-//			ans=Math.abs(n1-n2);
-//			break;
-//		case 3:
-//			ans=n1/n2;
-//			break;
-//		case 4:
-//			ans=n1*n2;
-//			break;
-//		default:
-//			ans=n1+n2;
-//		}
+		String s1=request.getParameter("number1");
+		String s2=request.getParameter("number2");
+		int n1=Integer.parseInt(s1);
+		int n2=Integer.parseInt(s2);
+		String op=request.getParameter("bt1");
+		int o=Integer.parseInt(op);
+		int ans;
+		switch(o) {
+		case 1:
+			ans=n1+n2;
+			break;
+		case 2:
+			ans=Math.abs(n1-n2);
+			break;
+		case 3:
+			ans=n1/n2;
+			break;
+		case 4:
+			ans=n1*n2;
+			break;
+		default:
+			ans=n1+n2;
+		}
 //		response.sendRedirect("MyServlet2");
 		RequestDispatcher dispatcher=request.getRequestDispatcher("MyServlet2");
 		dispatcher.forward(request, response);
 //		response.getWriter().append("ANS="+ans);
-//		response.sendRedirect("NewFile.jsp?ans="+ans);
+		response.sendRedirect("NewFile.jsp?ans="+ans);
 		
 	}
 
